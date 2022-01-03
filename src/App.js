cac89fcf81fee64c8ca0c7e71194475f534c44e5
Felix Saaro <felix@saaro.ch>
2022-01-03 22:01:42 +0100

    Add favicons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { FlagCalculator } from "./FlagCalculator";
 export const App = () => {
   return (
     <div className="inner">
-      <h1>CT1: Flags</h1>
+      <h1>Condition Flags Calculator</h1>
       <FlagCalculator />
     </div>
   );
